63d1f26b6ae37e8979812aea5a2cf9da6b2b85c6
lapoushko <vadim.dovgiy@mail.ru>
2024-04-21 22:25:17 +0500

    loading activity

--- app/src/main/java/com/example/audioguide/MainActivity.kt
+++ app/src/main/java/com/example/audioguide/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.example.audioguide
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.audioguide.dao.RouteDAO
+import com.example.audioguide.service.RouteServiceImpl
 
+/**
+ * Класс, запускающий меню экскурсий
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var routeListController: RouteListControllerImpl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //включить контент из activity main
+
+        val routeDao = RouteDAO()
+        val routeService = RouteServiceImpl(routeDao)
+        recyclerView = findViewById(R.id.recyclerView)
+        routeListController = RouteListControllerImpl(routeService, this )
+        routeListController.setupRecyclerView(recyclerView)
+//        for (i in routeService.getRoute()) {
+//            val toast = Toast.makeText(applicationContext, i.nameRoute, Toast.LENGTH_SHORT)
+//            toast.show()
+//        }
+
+//        val button = findViewById<Button>(R.id.button2)
+//        button.setOnClickListener {
+//            startActivity(Intent(this@MainActivity, Route::class.java)) //запуск activity route
+//        }
+        //В Android Studio код, который вы привели, использует ViewCompat.setOnApplyWindowInsetsListener
+        // для обработки системных отступов (insets) в приложении. Это позволяет вам
+        // динамически изменять отступы ваших представлений (views) в зависимости от
+        // системных элементов интерфейса, таких как статусная строка, панель навигации и т.д.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
