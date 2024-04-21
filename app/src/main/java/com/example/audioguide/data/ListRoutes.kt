34f3a86e6565c7db9786d9d29d7dd932e3da0ba5
lapoushko <vadim.dovgiy@mail.ru>
2024-04-21 22:22:39 +0500

    add list routes for tests

---
+++ app/src/main/java/com/example/audioguide/data/ListRoutes.kt
@@ -0,0 +1,23 @@
+package com.example.audioguide.data
+
+import com.example.audioguide.model.Route
+
+/**
+ * Список маршрутов для теста без подключения к базе данных
+ */
+class ListRoutes {
+    private val _route = arrayListOf(
+            Route("маршрут 1",
+        arrayListOf("Пункт 1", "Пункт 2"),
+        arrayListOf("https://storage.yandexcloud.net/images-audioguide/test_photo.jpg", "C:\\Users\\vadim\\AndroidStudioProjects\\AudioGuide\\app\\src\\main\\res\\drawable\\test_photo.jpg")),
+
+        Route("маршрут 2",
+            arrayListOf("Пункт 1", "Пункт 2"),
+            arrayListOf("https://storage.yandexcloud.net/images-audioguide/cat.jpg", "C:\\Users\\vadim\\AndroidStudioProjects\\AudioGuide\\app\\src\\main\\res\\drawable\\test_photo.jpg")),
+        Route("маршрут 3",
+            arrayListOf("Пункт 1", "Пункт 2"),
+            arrayListOf("https://storage.yandexcloud.net/images-audioguide/cat.jpg", "C:\\Users\\vadim\\AndroidStudioProjects\\AudioGuide\\app\\src\\main\\res\\drawable\\test_photo.jpg"))
+        )
+    val route: ArrayList<Route>
+        get() {return _route}
+}
