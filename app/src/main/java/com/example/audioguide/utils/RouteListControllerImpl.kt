a30229d8eebe721b4b0b5581cc38ac4f84de6c08
lapoushko <vadim.dovgiy@mail.ru>
2024-04-28 16:40:54 +0500

    Remove context

--- app/src/main/java/com/example/audioguide/utils/RouteListControllerImpl.kt
+++ app/src/main/java/com/example/audioguide/utils/RouteListControllerImpl.kt
@@ -1,6 +1,5 @@
 package com.example.audioguide
 
-import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.audioguide.service.RouteServiceImpl
@@ -10,10 +9,10 @@ import com.example.audioguide.utils.RouteMenuAdapter
 /**
  * Контроллер маршрутов
  */
-class RouteListControllerImpl(private val routeService: RouteServiceImpl, private val context: Context) : RouteListController {
+class RouteListControllerImpl(private val routeService: RouteServiceImpl) : RouteListController {
     override fun setupRecyclerView(recyclerView: RecyclerView) {
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
-        recyclerView.adapter = RouteMenuAdapter(routeService.getRoute(), context)
+        recyclerView.adapter = RouteMenuAdapter(routeService.getRoute())
     }
 }
