a1990dc160ef6e2d6c547dea281aea9bd132bcb4
lapoushko <vadim.dovgiy@mail.ru>
2024-04-28 16:41:05 +0500

    clean code

--- app/src/main/java/com/example/audioguide/model/Route.kt
+++ app/src/main/java/com/example/audioguide/model/Route.kt
@@ -1,41 +1,25 @@
 package com.example.audioguide.model
 
+import java.io.Serializable
+
 /**
  * Маршрут экскурсии
  */
-class Route(private val name: String,
-            private val info: List<String>,
-            private val paths: List<String>) {
-    /**
-     * Информация экскурсии
-     */
-    var listInformation = ArrayList<String>()
-        get() {
-            return info as ArrayList<String>
-        }
-        private set(value) {
-            field = value
-        }
+class Route(
+    private val id: Int,
+    private val name: String,
+    private val info: List<String>,
+    private val paths: List<String>
+) : Serializable {
+    val idRoute: Int
+        get() = id
 
-    /**
-     * Список всех путей к изображениям
-     */
-    var listPathsImage = ArrayList<String>()
-        get() {
-            return paths as ArrayList<String>
-        }
-        private set(value) {
-            field = value
-        }
+    val listInformation: List<String>
+        get() = info
 
-    /**
-     * Имя маршрута
-     */
-    var nameRoute: String = ""
-        get(){
-            return name
-        }
-        private set(value) {
-            field = value
-        }
+    val listPathsImage: List<String>
+        get() = paths
+
+    val nameRoute: String
+        get() = name
 }
