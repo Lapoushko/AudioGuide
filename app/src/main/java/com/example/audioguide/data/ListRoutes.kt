edbbd4731cf2d7752838e8d77e4150c1a8130a6e
lapoushko <vadim.dovgiy@mail.ru>
2024-04-28 16:41:38 +0500

    add id in list routes

--- app/src/main/java/com/example/audioguide/data/ListRoutes.kt
+++ app/src/main/java/com/example/audioguide/data/ListRoutes.kt
@@ -7,17 +7,17 @@ import com.example.audioguide.model.Route
  */
 class ListRoutes {
     private val _route = arrayListOf(
-            Route("маршрут 1",
+            Route(0,"маршрут 1",
         arrayListOf("Пункт 1", "Пункт 2"),
         arrayListOf("https://storage.yandexcloud.net/images-audioguide/test_photo.jpg", "C:\\Users\\vadim\\AndroidStudioProjects\\AudioGuide\\app\\src\\main\\res\\drawable\\test_photo.jpg")),
 
-        Route("маршрут 2",
+        Route(1,"маршрут 2",
             arrayListOf("Пункт 1", "Пункт 2"),
             arrayListOf("https://storage.yandexcloud.net/images-audioguide/cat.jpg", "C:\\Users\\vadim\\AndroidStudioProjects\\AudioGuide\\app\\src\\main\\res\\drawable\\test_photo.jpg")),
-        Route("маршрут 3",
+        Route(2,"маршрут 3",
             arrayListOf("Пункт 1", "Пункт 2"),
             arrayListOf("https://storage.yandexcloud.net/images-audioguide/cat.jpg", "C:\\Users\\vadim\\AndroidStudioProjects\\AudioGuide\\app\\src\\main\\res\\drawable\\test_photo.jpg"))
         )
     val route: ArrayList<Route>
-        get() {return _route}
+        get() {return this._route }
 }
