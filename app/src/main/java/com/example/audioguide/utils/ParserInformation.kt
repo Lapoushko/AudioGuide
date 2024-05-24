a3cbfaab5b9ba56b8cc07ea438866a9e6e890768
lapoushko <vadim.dovgiy@mail.ru>
2024-05-24 17:17:26 +0500

    Add parser

--- app/src/main/java/com/example/audioguide/utils/ParserInformation.kt
+++ app/src/main/java/com/example/audioguide/utils/ParserInformation.kt
@@ -1,16 +1,16 @@
 package com.example.audioguide.utils
 
-import com.example.audioguide.model.Route
-
 /**
  * Парсер информации о маршруте
  */
-class ParserInformation {
+class ParserInformation() {
     /**
      * Парсер информации
      */
-    fun parser(): List<Route>{
-        //TODO("Отпарсить информацию о маршруте" + "Определиться с передаваемым параметром")
-        return emptyList()
+    fun parser(info: String): List<String>{
+        if (info.isEmpty() || info.isBlank()){
+            return emptyList()
+        }
+        return info.split(';')
     }
 }
