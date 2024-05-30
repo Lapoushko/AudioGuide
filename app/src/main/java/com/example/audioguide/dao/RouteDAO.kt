45709c2d5f8a366bf43be578b4c6fe7ebc5656c8
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-05-31 02:33:49 +0500

    New fun in dao

--- app/src/main/java/com/example/audioguide/dao/RouteDAO.kt
+++ app/src/main/java/com/example/audioguide/dao/RouteDAO.kt
@@ -5,15 +5,24 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import com.example.audioguide.model.Route
 import com.example.audioguide.utils.ParserInformation
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
 
 /**
  * DAO для выполнения CRUD операций с БД маршрутов
  */
-class RouteDAO(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+class RouteDAO(private val context: Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
     private lateinit var listRoutes: List<Route>
     private lateinit var parserInformation: ParserInformation
-
+    init {
+        setPath()
+        copyDataBase()
+    }
     /**
      * Получение списка всех маршрутов из БД
      * @return возвращает список маршрутов
@@ -23,6 +32,7 @@ class RouteDAO(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         val db = this.readableDatabase
         val cursor = db.rawQuery("SELECT * FROM $TABLE_NAME", null)
         parserInformation = ParserInformation()
+
         while (cursor.moveToNext()) {
             val route = Route(
                 cursor.getInt(0),
@@ -30,7 +40,8 @@ class RouteDAO(context: Context, factory: SQLiteDatabase.CursorFactory?) :
                 cursor.getString(cursor.getColumnIndexOrThrow(DESCRIPTION)),
                 parserInformation.parser(cursor.getString(cursor.getColumnIndexOrThrow(INFO))),
                 parserInformation.parser(cursor.getString(cursor.getColumnIndexOrThrow(PATHS_IMAGE))),
-                parserInformation.parser(cursor.getString(cursor.getColumnIndexOrThrow(PATHS_AUDIO)))
+                parserInformation.parser(cursor.getString(cursor.getColumnIndexOrThrow(PATHS_AUDIO))),
+                parserInformation.parser(cursor.getString(cursor.getColumnIndexOrThrow(POINTS)))
             )
             list.add(route)
         }
@@ -40,20 +51,59 @@ class RouteDAO(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         return listRoutes
     }
 
+    private fun setPath(){
+        if (android.os.Build.VERSION.SDK_INT >= 17)
+            DATABASE_PATH = context.applicationInfo.dataDir + "/databases/"
+        else
+            DATABASE_PATH = "/data/data/" + context.packageName + "/databases/"
+    }
+
     /**
      * Создание базы данных
      *
      * @param db база данных
      */
     override fun onCreate(db: SQLiteDatabase?) {
+//        copyDataBase()
+//        setPath()
         val query = ("CREATE TABLE " + TABLE_NAME + " ("
                 + ID + " INTEGER PRIMARY KEY, " +
                 NAME + " TEXT," +
                 DESCRIPTION + " TEXT," +
                 INFO + " TEXT," +
                 PATHS_IMAGE + " TEXT," +
-                PATHS_AUDIO + " TEXT" + ")")
+                PATHS_AUDIO + " TEXT," +
+                POINTS + " TEXT" + ")")
         db?.execSQL(query)
+    }
+
+    @Throws(IOException::class)
+    private fun copyDBFile() {
+        val mInput: InputStream = context.assets.open(DATABASE_NAME)
+        val mOutput: OutputStream = FileOutputStream(DATABASE_PATH + DATABASE_NAME)
+        val mBuffer = ByteArray(1024)
+        var mLength: Int
+        while (mInput.read(mBuffer).also { mLength = it } > 0) mOutput.write(mBuffer, 0, mLength)
+        mOutput.flush()
+        mOutput.close()
+        mInput.close()
+    }
+
+    private fun copyDataBase() {
+        if (!checkDataBase()) {
+            this.readableDatabase
+            close()
+            try {
+                copyDBFile()
+            } catch (mIOException: IOException) {
+                throw Error("ErrorCopyingDataBase")
+            }
+        }
+    }
+
+    private fun checkDataBase(): Boolean {
+        val dbFile = File(DATABASE_PATH + DATABASE_NAME)
+        return dbFile.exists()
     }
 
     /**
@@ -73,9 +123,13 @@ class RouteDAO(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         private const val DATABASE_NAME = "audioguide.db"
 
         /**
+         * Database path
+         */
+        private var DATABASE_PATH = ""
+        /**
          * Database Version
          */
-        private const val DATABASE_VERSION = 4
+        private const val DATABASE_VERSION = 13
 
         /**
          * Table Name
@@ -111,6 +165,11 @@ class RouteDAO(context: Context, factory: SQLiteDatabase.CursorFactory?) :
          * Paths Audio
          */
         const val PATHS_AUDIO = "paths_audio"
+
+        /**
+         * Points
+         */
+        const val POINTS = "points"
     }
 
 }
